67c8fbc5af2b23b7ca69d7bd80933566ea29118e
unknown <pitkiller2.2@gmail.com>
2018-08-07 16:26:39 -0500

    Proyecto Base

---
+++ SCRIPTS/Cosechar_Politica.py
@@ -0,0 +1,44 @@
+import couchdb #Libreria de CouchDB (requiere ser instalada primero)
+from tweepy import Stream #tweepy es la libreria que trae tweets desde la API de Twitter (requiere ser instalada primero)
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json #Libreria para manejar archivos JSON
+
+
+ckey = "Wee6fqwfWRZaTsNm2lNdeZemB"
+csecret = "4k5VQ9rS9ivJHBmAGKBFy29vWu2oBCnzMA9xxq5kSlK1bGmuGk"
+atoken = "767136071817826304-aOjltLigv68ywKrYnHOszlsaNuHBCAU"
+asecret = "JPnKD9Ss20vCFyCOpoTYZn3haDC4repX9B7GzVZHWTi2N"
+
+class listener(StreamListener):
+    
+    def on_data(self, data):
+        dictTweet = json.loads(data)
+        try:
+            dictTweet["_id"] = str(dictTweet['id'])
+           
+            doc = db.save(dictTweet) #Aqui se guarda el tweet en la base de couchDB
+            print ("Guardado" + "=> " + dictTweet["_id"])
+        except:
+            print ("Documento ya existe")
+            pass
+        return True
+    
+    def on_error(self, status):
+        print (status)
+        
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+
+#Setear la URL del servidor de couchDB
+server = couchdb.Server('http://localhost:5984/')
+try:
+    #Si no existe la Base de datos la crea
+    db = server.create('restaurantes')
+except:
+    #Caso contrario solo conectarse a la base existente
+    db = server['restaurantes']
+    
+#Aqui se define el bounding box con los limites geograficos donde recolectar los tweets
+twitterStream.filter(track=["quito", "guayaquil", "cuenca", "ambato", "ibarra"])
---
+++ SCRIPTS/cosechar_tweets Estadios.py
@@ -0,0 +1,52 @@
+
+import couchdb #Libreria de CouchDB (requiere ser instalada primero)
+from tweepy import Stream #tweepy es la libreria que trae tweets desde la API de Twitter (requiere ser instalada primero)
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json #Libreria para manejar archivos JSON
+
+
+ckey = "a3EFsN0kUu04QtdUJlFdSBrHH"
+csecret = "uXXkD81WVnhLk0M8tdNb8jDXAsQhIGzKSFcUQRlDShWFl9Wzg3"
+atoken = "2593813213-Fi5yWsk71uKcGmDconMaY8obRVV4A5QXmP9TVQU"
+asecret = "4nXx4BNOE9QK5MMW1TxXAxz3U69hBdB1Y1caXp2rJ0Nz0"
+
+class listener(StreamListener):
+    
+    def on_data(self, data):
+        dictTweet = json.loads(data)
+        try:
+            dictTweet["_id"] = str(dictTweet['id'])
+           
+            doc = db.save(dictTweet) #Aqui se guarda el tweet en la base de couchDB
+            print ("Guardado " + "=> " + dictTweet["_id"])
+        except:
+            print ("Documento ya existe")
+            pass
+        return True
+    
+    def on_error(self, status):
+        print (status)
+        
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+
+#Setear la URL del servidor de couchDB
+server = couchdb.Server('http://localhost:5984/')
+try:
+    #Si no existe la Base de datos la crea
+    db = server.create('estadios')
+except:
+    #Caso contrario solo conectarse a la base existente
+    db = server['estadios']
+    
+#Aqui se define el bounding box con los limites geograficos donde recolectar los tweet
+twitterStream.filter(track=["Monumental Isidro Romero Carbo",
+                            "Alberto Spencer",
+                            "Rodrigo Paz Delgado",
+                            "Olímpico Atahualpa",
+                            "Reales Tamarindos",
+                            "Alejandro Serrano Aguilar"])
+
+
